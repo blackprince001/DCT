a63535112e9fe2ab3eea36137fcf644d0686db25
blackprince001 <appiahboaduprince@gmail.com>
2024-11-17 10:44:52 +0000

    add packet size to signature

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
-mod network;
-
 use std::error::Error;
 
-use network::run;
 use sysinfo::Networks;
+
+mod network;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let networks = Networks::new_with_refreshed_list();
     let interface = String::from("en0");
     let scrape_time = 1000; // 1 second
+    let packetsize = network::types::DataSize::Kilobyte;
 
-    run(networks, &interface, scrape_time)?;
+    network::net::run(networks, &interface, scrape_time, packetsize)?;
     Ok(())
 }
